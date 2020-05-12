f08105f31a8fa970c205a46a9b7246c9a3d8e2b1
Diogenis <dio.tsolakou@hotmail.com>
2020-05-12 21:45:18 +0300

    Fixes

---
+++ app/src/main/java/spotifyPackage/MainActivity.java
@@ -0,0 +1,14 @@
+package spotifyPackage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import spotifyPackage.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
