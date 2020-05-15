92e9ed4fe2461b24a0237523a1f74faef43f2eee
DioTsolakou <32932164+diotsolakou@users.noreply.github.com>
2020-05-15 17:52:13 +0300

    Moved search/find methods to a Utilities class in a Utilities package

--- app/src/main/java/spotifyPackage/Activities/MainActivity.java
+++ app/src/main/java/spotifyPackage/Activities/MainActivity.java
@@ -2,15 +2,52 @@ package spotifyPackage.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
 import spotifyPackage.R;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView artistList;
+    private AdapterView.OnItemClickListener listClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String artistName = (String) artistList.getItemAtPosition(position);
+            Toast.makeText(MainActivity.this, artistName + " was selected",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        artistList = findViewById(R.id.listView);
+
+        //test arraylist , will be replaced with artist's arraylist
+        ArrayList<String> places = new ArrayList<String>();
+        places.add("Buenos Aires");
+        places.add("Córdoba");
+        places.add("La Plata");
+        places.add("Buenos Aires");
+        places.add("Córdoba");
+        places.add("La Plata");
+        places.add("Buenos Aires");
+        places.add("Córdoba");
+        places.add("La Plata");
+        places.add("Buenos Aires");
+        places.add("Córdoba");
+        places.add("La Plata");
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, places);
+        artistList.setAdapter(myAdapter);
+        artistList.setOnItemClickListener(listClick);
     }
 
     @Override
